336cfa3375392ba0cfa9d503eb7873db116b4479
Speed <154397637+Phuc710@users.noreply.github.com>
2025-11-08 13:19:47 +0700

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,21 +1,25 @@
-
 module.exports = {
-    enabled: true, 
-    lavalink: [
-      {
-        name: "Serenetia V4 - Primary",
-        password: "https://dsc.gg/ajidevserver",
-        host: "lavalinkv4.serenetia.com",
-        port: 443,
-        secure: true
-      },
-      {
-        name: "Amane V4 (EU) - Backup",
-        password: "https://dsc.gg/ajidevserver",
-        host: "lava-v4.ajieblogs.eu.org",
-        port: 443,
-        secure: true
-      },
-    ]
+  lavalink: [
+    {
+      name: "1",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "3",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+    },
+    {
+      name: "2 - Backup",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    }
+  ]
 };
-
